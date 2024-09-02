e91dd6aa78d07f3ab9e86eed22d6c597bd00f5bd
Wilbert <wilbertirece002@gmailcom>
2024-09-02 14:02:05 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intVar;
+            double doubleVar;
+            char charVar;
+            string stringVar;
+            bool boolVar;
+            int num1, num2;
+
+            intVar = 1;
+            doubleVar = 2.3;
+            charVar = 'a';  
+            stringVar = "hello";
+            boolVar = false;
+            num1 = 1;
+            num2 = 2;
+
+
+            Console.WriteLine("Integer Var:" + intVar);
+            Console.WriteLine("double Var:" + doubleVar);
+            Console.WriteLine("char Var:" + charVar);
+            Console.WriteLine("String Var:" + stringVar);
+            Console.WriteLine("Bool Var: "+ boolVar);
+
+            Console.ReadKey();
+        }
+    }
+}
